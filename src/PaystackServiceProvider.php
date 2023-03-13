0e10e8019171d850c0e2408cfc9c209e31f36af3
mayejacob <mayejacob3@gmail.com>
2023-03-14 00:15:46 +0100

    new commit

--- src/PaystackServiceProvider.php
+++ src/PaystackServiceProvider.php
@@ -11,17 +11,10 @@ class PaystackServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/config/paystack.php' => config_path('paystack.php')
         ]);
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/views', 'paystack');
     }
     public function register()
     {
-        // $this->app->bind('laravel-paystack', function () {
-        //     return new Paystack;
-        // });
+        
     }
-    // public function provides()
-    // {
-    //     return ['laravel-paystack'];
-    // }
+    
 }
